997ac5b60ba92814bbbda5766539afe21e9b9761
Dan Danciu <dan.danciu@bea-services.com>
2018-09-24 22:53:57 +0300

    fixed multiple issues

--- img-resize/img_threading_better.py
+++ img-resize/img_threading_better.py
@@ -12,31 +12,20 @@ import timeit
 def resize(file):
     print(f'Resizing {file} in thread {threading.get_ident()}')
     file_name, ext = file.split('.')
-    os.chdir(npath1)
+
+    file = npath1+'\\'+file
     im = Image.open(file)
 
     Hpixel = im.size[0]/(10)
     Wpixel = im.size[1]/(10)
-    new_file = file_name+'t.jpg'
+    new_file = npath2+'\\'+file_name+ext
     im.resize((int(Hpixel),int(Wpixel))).save(new_file, "JPEG")
-    nim = Image.open("%st.jpg" %file_name)
+    nim = Image.open(new_file)
     print ('\tresized image')
     print ('\t\tOld Size: ' ,im.size)
     print ('\t\tNew size: ' ,nim.size)
     im.close()
     nim.close()
-    os.chdir(npath1)
-    shutil.copy2(new_file, npath2)
-    print ('\tmoved image')
-    os.chdir(npath2)
-    if fnmatch.fnmatch(new_file,'*t.jpg'):
-        os.rename(new_file, file_name+'.jpg')
-        print ('\trenamed image')
-    os.chdir(npath1)
-    new_path = npath1+'\\'+new_file
-    if fnmatch.fnmatch(new_file,'*t.jpg'):
-        os.remove(new_path)
-        print ('\tremoved copy')
 
 if __name__ == '__main__':
     start = timeit.default_timer()
